95524afc1995b8cfd0807f4f75f04dae75d4bbd4
Alexander <troshiy2011@mail.ru>
2022-08-17 15:10:55 +0300

    media

--- csvproject/csvapp/models.py
+++ csvproject/csvapp/models.py
@@ -5,8 +5,14 @@ class CsvModel(models.Model):
     ISBN = models.BigIntegerField()
     Book_Title = models.CharField(max_length=255,null=True)
     Book_Author = models.CharField(max_length=255,null=True)
-    Year_Of_Publication = models.DateField(auto_now=True)
+    Year_Of_Publication = models.BigIntegerField(null=True)
     Publisher = models.CharField(max_length=255,null=True)
     Image_URL_S = models.CharField(max_length=255,null=True)
     Image_URL_M = models.CharField(max_length=255,null=True)
     Image_URL_L = models.CharField(max_length=255,null=True)
+
+    def __str__(self) -> str:
+        return self.Book_Title
+        
+#from csvapp.models import *
+#GETED, created = CsvModel.objects.get_or_create(ISBN=12356,Book_Title='JOPA')
--- csvproject/csvapp/views.py
+++ csvproject/csvapp/views.py
@@ -19,16 +19,20 @@ class CsvToModel(TemplateView):
     #                 last_name=row[1],
     #                 middle_name=row[2],
     #                 )
-        # return HttpResponse('result')    
-        
-    for row in csv_reader:
-        CsvModel.objects.get_or_create(
-        ISBN=row[0],
-        last_name=row[1],
-        email=row[2],
-        organisation=row[3],
-        enrolled=row[4],
-        last_booking=row[5],
-        credits_total=row[6],
-        credits_balance=row[7],
-        )   
+        # return HttpResponse('result')  
+
+
+    # with open('mpathtofile') as f:
+    #         reader = csv.reader(f)
+    #         for row in csv_reader:
+    #             CsvModel.objects.get_or_create(
+    #             ISBN=row[0],
+    #             last_name=row[1],
+    #             email=row[2],
+    #             organisation=row[3],
+    #             enrolled=row[4],
+    #             last_booking=row[5],
+    #             credits_total=row[6],
+    #             credits_balance=row[7],
+    #             )   
+    #         #bulk_create(batch_size=10000)
