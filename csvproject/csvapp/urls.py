e501cd7d9089d284a219e67bddf281cd8d74c564
Alexander <troshiy2011@mail.ru>
2022-08-19 15:31:43 +0300

    debug toolbar

--- csvproject/csvapp/urls.py
+++ csvproject/csvapp/urls.py
@@ -1,8 +1,9 @@
-from django.urls import  path
+from django.urls import  include, path
 from .views import *
 
 
 urlpatterns = [
+    path('__debug__/', include('debug_toolbar.urls')),
     path('',FileUploadView.as_view(),name='home'),
     path('parse/',ParseView.as_view(),name='parse'),
     path('data/',DataView.as_view(),name='data'),
--- csvproject/csvproject/settings.py
+++ csvproject/csvproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'csvapp.apps.CsvappConfig',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'csvproject.urls'
@@ -128,3 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
