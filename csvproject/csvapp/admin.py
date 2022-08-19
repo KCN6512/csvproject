0ce34596719c0fd9276baba53db39939653f3a4b
Alexander <troshiy2011@mail.ru>
2022-08-19 17:12:16 +0300

    added file upload through the admin panel

--- csvproject/csvapp/admin.py
+++ csvproject/csvapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+class CsvModelAdmin(admin.ModelAdmin):
+    list_display = ('file',)
+    search_fields = ('file',)#нужно передать кортеж поэтому запятая,
+
+
+admin.site.register(FileUploadModel,CsvModelAdmin)
